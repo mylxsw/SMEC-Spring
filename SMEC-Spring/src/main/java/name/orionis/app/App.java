ee1dbb37eb7d5f517c3ba046809f8f0f09cacd1e
orionis <orionis@orionis-PC>
2013-05-12 22:16:55 +0800

    增加单元测试

--- SMEC-Spring/src/main/java/name/orionis/app/App.java
+++ SMEC-Spring/src/main/java/name/orionis/app/App.java
@@ -1,5 +1,8 @@
 package name.orionis.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import name.orionis.helper.cache.Cached;
 
 import org.springframework.stereotype.Component;
@@ -7,9 +10,38 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class App {
-	@Cached
+	@Cached(value="cache_demo", expiration=30)
 	public String getCacheObject(String key, String key2){
 		System.out.println("正在生成结果...");
 		return key + ":" +  key2;
 	}
+	
+	@Cached(value="cache_demo_clear")
+	public String getCacheObject2(String key1, int key2){
+		System.out.println("正在生成结果...");
+		return "你好啊";
+	}
+	@Cached(value="cache_demo_clear", reload=true)
+	public void clearCacheObject2(String key1, int key2){
+		System.out.println("清理缓存...");
+	}
+	
+	@Cached(value="cache_demo_3", appendParam=false)
+	public Map<String , String> getCacheObject3(){
+		System.out.println("正在生成结果...");
+		return new HashMap<String ,String>();
+	}
+	
+	
+	@Cached(value="cache_demo_4", identifyParams={"key2"})
+	public String getCacheObject4(String key1, String key2 ,String key3){
+		System.out.println("正在生成结果...");
+		return key1 + key2 + key3;
+	}
+	
+	@Cached(value="cache_demo*", reload=true)
+	public void clearAllCache(){
+		System.out.println("清理全部缓存...");
+	}
+	
 }
--- SMEC-Spring/src/test/java/name/orionis/app/SpringTest/AppTest.java
+++ SMEC-Spring/src/test/java/name/orionis/app/SpringTest/AppTest.java
@@ -12,11 +12,49 @@ public class AppTest extends SpringTest{
 	private App app;
 	
 	@Test
-	public void testApp(){
+	public void testApp() throws InterruptedException{
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		Thread.sleep(25);
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		Thread.sleep(30);
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		debug( app.getCacheObject("mylxsw", "orionis") );
+	}
+	@Test
+	public void testApp2(){
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 6));
+		debug( app.getCacheObject2("what", 6));
+		
+		app.clearCacheObject2("what", 5);
+		
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 6));
+		debug( app.getCacheObject2("what", 6));
+	}
+	
+	@Test
+	public void testApp3() {
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 5));
 		debug( app.getCacheObject("mylxsw", "orionis") );
 		debug( app.getCacheObject("mylxsw", "orionis") );
 		debug( app.getCacheObject("mylxsw", "orionis") );
 		debug( app.getCacheObject("mylxsw", "orionis") );
+		
+		debug("------------------------------------");
+		app.clearAllCache();
+		debug("------------------------------------");
+		
+		debug( app.getCacheObject2("what", 5));
+		debug( app.getCacheObject2("what", 5));
 		debug( app.getCacheObject("mylxsw", "orionis") );
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		debug( app.getCacheObject("mylxsw", "orionis") );
+		
 	}
 }
